6b868da2fe67c29fcccb775518b4751277b8d5a2
saritasubhra <saritasubhra1@gmail.com>
2025-10-21 17:20:45 +0530

    frontend authentication logic added

---
+++ client/context/AuthContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axios from "../lib/axios";
+
+const AuthContext = createContext();
+
+function AuthProvider({ children }) {
+  const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    async function fetchProfile() {
+      try {
+        const res = await axios.get("/auth/profile");
+        setAuth(res.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchProfile();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("AuthContext used outside of provider");
+  return context;
+}
+
+export default AuthProvider;
